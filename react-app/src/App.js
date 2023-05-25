954f277eb7611911ee9fb798d6f4a6a19766c1a1
omalley242 <omalley242@gmail.com>
2023-05-25 17:31:28 +0100

    starting basic react file format

---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div>
+      <img src="https://live.staticflickr.com/5515/11003816936_08e72de26e_z.jpg" className="Testing" alt="test"/>
+    </div>
+  );
+}
+
+export default App;
