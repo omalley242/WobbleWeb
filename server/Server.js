a84a361e3a7c0c180a867e72d020c82b3d29de53
omalley242 <omalley242@gmail.com>
2023-05-26 13:57:57 +0100

    changing how static files are served within the express server

--- server/Server.js
+++ server/Server.js
@@ -3,9 +3,6 @@
 const express = require('express');
 const app = express();
 
-//tell the server to use static files within the react-build section
-app.use(express.static('../react-app/build'));
-
 //a library to locate the current path
 const path = require('path');
 
@@ -14,8 +11,11 @@ app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..','react-app', 'build', 'index.html'))
 });
 
+//tell the server to use static files within the react-build section
+app.use(express.static('../react-app/build/'));
+
 //set the port number to listen on
-const PORT = 5000;
+const PORT = 8080;
 console.log("Server has been started on port", PORT);
 
 //launch the server
