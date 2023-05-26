809ee768531dd8c6fd0171b70367604d629d575e
omalley242 <omalley242@gmail.com>
2023-05-26 15:25:41 +0100

    feat: adding basic api for esp comms

--- server/Server.js
+++ server/Server.js
@@ -14,6 +14,40 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..','react-app', 'build', 'index.html'))
 });
 
+
+//Handle requests from the esp32 
+app.get('/esp', (req, res) => {
+    req.accepts('json, text');
+    console.log("-----Esp32 Message Recieved-----");
+    console.log("Message of type GET");
+    res.status(200).json({direction: "left"});
+});
+
+//Handle requests from the esp32 
+app.post('/esp', (req, res) => {
+    req.accepts('json, text');
+    console.log("-----Esp32 Message Recieved-----");
+    console.log("Message of type GET");
+    res.status(200).json({direction: "left"});
+});
+
+//Handle requests from the esp32 
+app.put('/esp', (req, res) => {
+    req.accepts('json, text');
+    console.log("-----Esp32 Message Recieved-----");
+    console.log("Message of type PUT");
+    res.status(200).json({direction: "left"});
+});
+
+//Handle requests from the esp32 
+app.delete('/esp', (req, res) => {
+    req.accepts('json, text');
+    console.log("-----Esp32 Message Recieved-----");
+    console.log("Message of type DELETE");
+    res.status(200).json({direction: "left"});
+});
+
+
 //set the port number to listen on
 const PORT = 5000;
 console.log("Server has been started on port", PORT);
