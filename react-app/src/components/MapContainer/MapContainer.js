f04e83c92d796a3fd296201b86388c97d6f09cec
omalley242 <omalley242@gmail.com>
2023-05-29 21:47:25 +0100

    adding baic handeling of the database data

--- react-app/src/components/MapContainer/MapContainer.js
+++ react-app/src/components/MapContainer/MapContainer.js
@@ -2,10 +2,11 @@ import './MapContainer.css'
 
 const MapContainer = ({data}) => {
    
+    console.log(data);
     //a constant function that takes an input "data" and returns a list of Map-Item elements with a surronding div, called Map-Container
     const MapItems = data.map((item, index) => (
-        <div key={index} className='Map-Item' style={{}}>
-            {item.content}
+        <div key={index} className='Map-Item' style={{position: 'absolute', left: item.X_Pos, bottom: item.Y_Pos, height: 30, width: 30}}>
+            {item.Con_Num}
         </div>
     ));
 
