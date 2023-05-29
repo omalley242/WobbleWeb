77c7e495e6e7c2b1c5719f3ac40ec34de0e8c43f
omalley242 <omalley242@gmail.com>
2023-05-29 22:49:19 +0100

    fixing svg loading issues

--- react-app/src/components/MapContainer/MapContainer.js
+++ react-app/src/components/MapContainer/MapContainer.js
@@ -1,12 +1,12 @@
 import './MapContainer.css'
-import {ReactComponenet as Node} from '../../node.svg'; 
+import Node from '../../node.svg'; 
 const MapContainer = ({data}) => {
    
     console.log(data);
     //a constant function that takes an input "data" and returns a list of Map-Item elements with a surronding div, called Map-Container
     const MapItems = data.map((item, index) => (
         <div key={index} className='Map-Item' style={{position: 'absolute', left: item.X_Pos, bottom: item.Y_Pos}}>
-            <Node >{item.Con_Num}</Node>            
+            <img src={Node} alt="Node Icon">{item.Con_Num}</img>            
         </div>
     ));
 
