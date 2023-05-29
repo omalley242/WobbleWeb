5e0b8587f44f1e9cd79589a95534227458b0264e
omalley242 <omalley242@gmail.com>
2023-05-29 22:46:37 +0100

    adding node svg

--- react-app/src/components/MapContainer/MapContainer.js
+++ react-app/src/components/MapContainer/MapContainer.js
@@ -1,17 +1,17 @@
 import './MapContainer.css'
-
+import {ReactComponenet as Node} from '../../node.svg'; 
 const MapContainer = ({data}) => {
    
     console.log(data);
     //a constant function that takes an input "data" and returns a list of Map-Item elements with a surronding div, called Map-Container
     const MapItems = data.map((item, index) => (
         <div key={index} className='Map-Item' style={{position: 'absolute', left: item.X_Pos, bottom: item.Y_Pos}}>
-            <circle cx="50" cy="50" r="40" stroke="black" stroke-width="3" fill="green">{item.Con_Num}</circle>            
+            <Node >{item.Con_Num}</Node>            
         </div>
     ));
 
     return (
-        <div className='Map-Container' style={{display: 'flex', position: 'relative', height: 800, width: 1000, flexWrap: 'wrap', border: '3px solid #222'}}>
+        <div className='Map-Container' style={{display: 'flex', position: 'relative', height: 800, width: 1000, flexWrap: 'wrap', border: '3px solid #222', margin: 50}}>
             {MapItems}
         </div>
     )
