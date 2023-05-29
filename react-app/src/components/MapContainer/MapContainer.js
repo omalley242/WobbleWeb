04e17cfddcef9ba967508c747e6c3ebf29d49a7a
omalley242 <omalley242@gmail.com>
2023-05-29 22:32:55 +0100

    styling changes

--- react-app/src/components/MapContainer/MapContainer.js
+++ react-app/src/components/MapContainer/MapContainer.js
@@ -5,13 +5,13 @@ const MapContainer = ({data}) => {
     console.log(data);
     //a constant function that takes an input "data" and returns a list of Map-Item elements with a surronding div, called Map-Container
     const MapItems = data.map((item, index) => (
-        <div key={index} className='Map-Item' style={{position: 'relative', left: item.X_Pos, bottom: item.Y_Pos, height: 30, width: 30}}>
+        <div key={index} className='Map-Item' style={{position: 'absolute', left: item.X_Pos, bottom: item.Y_Pos, height: 30, width: 30}}>
             {item.Con_Num}
         </div>
     ));
 
     return (
-        <div className='Map-Container' style={{display: 'flex', height: 800, width: 1000, flexWrap: 'wrap', border: '3px solid #222'}}>
+        <div className='Map-Container' style={{display: 'flex', position: 'relative', height: 800, width: 1000, flexWrap: 'wrap', border: '3px solid #222'}}>
             {MapItems}
         </div>
     )
