318125eaa7ec0eeec90ca41fa7e3a115efe965ad
omalley242 <omalley242@gmail.com>
2023-05-29 21:25:25 +0100

    testing database fetching for updating react components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
 import MapContainer from './components/MapContainer/MapContainer';
+import React, {useState} from 'react';
+import ReactPolling from 'react-polling/lib/ReactPolling';
 
 function App() {
 
-  let data = [];
-  for(let i=0; i< 3025; i++){
-    data.push({content: ""});
+  const [nodeData, updateNodeData] = useState([[]]);
+
+  const fetchNodeData = () => {
+    //location.host gives the server ip
+    return fetch(`http://${window.location.host}/nodes`);
   }
+
+  console.log(fetch(`http://${window.location.host}/nodes`));
+  //Fetch all nodes from the database, on load
+
+
+  //pass the data into the map container and create a component for each
+
+  //use the data for css positioning
+
+  //Repeat for each path
+
+  //poll the database for changes
+
+  //on change re-render the components within the container
+
+  let data = [{content: "test"}];
 
   return (
     <div style={{display: 'flex', width: '100%', height: '100%', justifyContent: 'center', alignContent: 'center', padding: '0', margin: '0'}}>
