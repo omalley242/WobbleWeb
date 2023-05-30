6deb2437e4b0bec8f0f0ba92eaf2bfb359157756
omalley242 <omalley242@gmail.com>
2023-05-30 13:00:39 +0100

    test

--- server/Server.js
+++ server/Server.js
@@ -96,7 +96,7 @@ function main_server(database_connection) {
         console.log("Adding New Node");
         console.log(req);
         console.log(req.body);
-
+        let reqbody = req.body;
         let nodeJson = reqbody.Node[0];
         let pathsJson = reqbody.Paths;
 
