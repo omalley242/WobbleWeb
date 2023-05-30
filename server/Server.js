f5fe94051e1dc5c7f0e9a99b8042a1de5be0da35
omalley242 <omalley242@gmail.com>
2023-05-30 13:17:57 +0100

    test

--- server/Server.js
+++ server/Server.js
@@ -95,7 +95,8 @@ function main_server(database_connection) {
     app.post('/add/node', bodyParser.json(), (req, res) => {
         console.log("Adding New Node");
         console.log(req.body);
-
+        console.log(req);
+        
         let reqbody = req.body;
         let nodeJson = reqbody.Node;
         let pathsJson = reqbody.Path;
