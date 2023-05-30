1cd7d7e9c0ca529972f6d151a1eca70d9801a4fa
omalley242 <omalley242@gmail.com>
2023-05-30 13:47:04 +0100

    test

--- server/Server.js
+++ server/Server.js
@@ -48,6 +48,13 @@ function main_server(database_connection) {
         console.log("Successful database connection");
     });
 
+    function defaultContentTypeMiddleware (req, res, next) {
+        req.headers['content-type'] = req.headers['content-type'] || 'application/json';
+        next();
+      }
+      
+    app.use(defaultContentTypeMiddleware);
+
     //tell the server to use static files within the react-build section
     app.use(express.static(path.join(__dirname, '../react-app/build')));
 
@@ -96,7 +103,7 @@ function main_server(database_connection) {
         console.log("Adding New Node");
         console.log(req.body);
         console.log(req);
-        
+        2
         let reqbody = req.body;
         let nodeJson = reqbody.Node;
         let pathsJson = reqbody.Path;
