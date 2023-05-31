d70625faec6956b489d5244d855a637da3db8b87
omalley242 <omalley242@gmail.com>
2023-05-31 13:16:59 +0100

    test

--- server/Server.js
+++ server/Server.js
@@ -180,11 +180,11 @@ function main_server(database_connection) {
 
 
         // Start_ID | End_ID | Heading From Start | Distance |
-        pathsJson.map((pathItem) => {
-            database_connection.query(`INSERT INTO Paths VALUES (${pathItem.Start_Id},0,0,0,0)`, function(err, result, fields) {
-            if (err) throw err;
-            });
-        });
+        // pathsJson.map((pathItem) => {
+        //     database_connection.query(`INSERT INTO Paths VALUES (${pathItem.Start_Id},0,0,0,0)`, function(err, result, fields) {
+        //     if (err) throw err;
+        //     });
+        // });
 
         res.status(200).json("Recieved shiz");
         console.log("Added new Node");
