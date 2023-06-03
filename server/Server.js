a9d7d252d8bbb9727373d22ce2fc0f17e7410f47
omalley242 <omalley242@gmail.com>
2023-06-03 15:29:15 +0100

    changing angles between points solutions

--- server/Server.js
+++ server/Server.js
@@ -45,7 +45,7 @@ var BC=Math.sqrt( Math.pow((XB-XC),2) + Math.pow((YB-YC),2) );
 
 //find angles between points
 var ANG_A = Math.acos( (Math.pow(AB, 2) + Math.pow(AC,2) - Math.pow(BC,2))/(2*AB*AC) );
-var ANG_B = Math.acos( (Math.pow(AB, 2) + Math.pow(BC,2) - Math.pow(AC,2))/(2*AB*BC) );
+var ANG_B = 2*Math.PI - Math.acos( (Math.pow(AB, 2) + Math.pow(BC,2) - Math.pow(AC,2))/(2*AB*BC) );
 var ANG_C = Math.acos( (Math.pow(AC, 2) + Math.pow(BC,2) - Math.pow(AB,2))/(2*AC*BC) );
 
 //find cotangent of landmark angles
