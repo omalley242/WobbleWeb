8a679ec56b68921517b2be19946fb02aab96f358
omalley242 <omalley242@gmail.com>
2023-06-20 21:54:07 +0100

    styling

--- react-app/src/components/MapContainer/MapContainer.js
+++ react-app/src/components/MapContainer/MapContainer.js
@@ -47,7 +47,7 @@ const MapContainer = ({nodeData, pathData}) => {
                     {MapItems}
                 </Xwrapper>
             </span>
-            <div style={{overflowY: 'scroll', width: '90vw', height: '6em'}}>
+            <div style={{overflowY: 'scroll', width: '100%', height: '6em'}}>
                 {TextBox}
             </div>
         </span>
