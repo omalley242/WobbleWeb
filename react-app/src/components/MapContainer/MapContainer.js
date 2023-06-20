82aba6c1590bf7f2a132b0c3ab2b75be73b6cd31
omalley242 <omalley242@gmail.com>
2023-06-20 21:27:00 +0100

    styling

--- react-app/src/components/MapContainer/MapContainer.js
+++ react-app/src/components/MapContainer/MapContainer.js
@@ -36,15 +36,17 @@ const MapContainer = ({nodeData, pathData}) => {
     });
     
     return (
-        <span style= {{width: '100%', height: '100%'}}>
-            <div className='Map-Container' style={{display: 'flex', position: 'relative', height: '53.3vw', width: '80vw', flexWrap: 'wrap', border: '3px solid #222', margin: '5vh', backgroundColor: '#444'}}>
-                    <Xwrapper>
-                        {PathItems}
-                        {MapItems}
-                    </Xwrapper>
-            </div>
-            pathData
-        </span>
+        <div className='Map-Container' style={{display: 'flex', position: 'relative', height: '53.3vw', width: '80vw', flexWrap: 'wrap', border: '3px solid #222', margin: '5vh', backgroundColor: '#444'}}>
+            <span style= {{width: '100%', height: '100%'}}>
+                <Xwrapper>
+                    {PathItems}
+                    {MapItems}
+                </Xwrapper>
+            </span>
+            <span>
+                {TextBox}
+            </span>
+        </div>
 
     )
 }
