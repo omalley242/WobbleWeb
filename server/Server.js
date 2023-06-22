4edc8fe9f44d94499dfb8aaee4ba267c847b433c
omalley242 <omalley242@gmail.com>
2023-06-22 23:59:59 +0100

    auto-testing

--- server/Server.js
+++ server/Server.js
@@ -298,7 +298,7 @@ function main_server(database_connection) {Introduction:
 
             nodeData[node] = nodeData[node].map((path) => {
                 let node_dict = {};
-                return (path.StartId == node) ? (node_dict[(path.EndId + "test")]=path.Distance) : (node_dict[path.StartId] = path.Distance)
+                return (path.StartId == node) ? (node_dict[(path.EndId + "test")]=path.Distance) : (node_dict[(path.StartId + "test")] = path.Distance)
             });
             // console.log(`${node}: ${nodeData[node][0].EndId, nodeData[node][0].Distance}`);
         }
