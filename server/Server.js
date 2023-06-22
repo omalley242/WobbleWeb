0c3283dbb3fc23ace42af861d2b041f175e0075d
omalley242 <omalley242@gmail.com>
2023-06-23 00:28:15 +0100

    auto-testing

--- server/Server.js
+++ server/Server.js
@@ -295,14 +295,12 @@ function main_server(database_connection) {Introduction:
         let graph = new Object();
         // nodeData.forEach(node => node.keys());
         for (node in nodeData) {
-            // let temp_dict = {};
             for (paths in nodeData[node]) {
                 let EndId = paths.EndId;
                 let Distance = paths.Distance;
                 let StartId = paths.StartId;
-                (paths.StartId == node) ? {[EndId] : Distance} : {[StartId] : Distance};
+                nodeData[node] = (paths.StartId == node) ? {[EndId] : Distance} : {[StartId] : Distance};
             }
-            nodeData[node] = temp_dict;
             // console.log(`${node}: ${nodeData[node][0].EndId, nodeData[node][0].Distance}`);
         }
 
