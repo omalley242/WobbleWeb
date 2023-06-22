8d4e04b54e4d1584f95f60d2446417aa929448c6
omalley242 <omalley242@gmail.com>
2023-06-22 23:28:22 +0100

    auto-testing

--- server/Server.js
+++ server/Server.js
@@ -295,7 +295,7 @@ function main_server(database_connection) {Introduction:
         let graph = new Object();
         // nodeData.forEach(node => node.keys());
         for (node in nodeData) {
-            nodeData[node].map((path) => (path.StartId == node) ? new Object(path.EndId, path.Distance) : new Object(path.StartId, path.Distance));
+            nodeData[node].map((path) => (path.StartId == node) ? "test1" : "test2");
             console.log(nodeData);
 
             // console.log(`${node}: ${nodeData[node][0].EndId, nodeData[node][0].Distance}`);
