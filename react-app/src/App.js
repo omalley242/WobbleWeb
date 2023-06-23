fa28d77df70953e6f399b9bb224edd967259fac8
omalley242 <omalley242@gmail.com>
2023-06-23 11:12:46 +0100

    auto-testing

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -16,7 +16,7 @@ function App () {
     websocket.onmessage = (message) => {
       console.log(message);
       console.log(websocketData);
-      updateWebsocketData(message.data);    
+      updateWebsocketData(message.data.JSON);    
     }
 
     //Movement Speed / Distance difference
