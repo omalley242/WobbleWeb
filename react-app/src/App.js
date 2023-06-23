1cad5196c5d0ca1c2e8ddd2614c592da4bebae58
omalley242 <omalley242@gmail.com>
2023-06-23 02:14:58 +0100

    auto-testing

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,6 +5,49 @@ import ReactPolling from 'react-polling/lib/ReactPolling';
 
 function App(){
 
+//dictionary holding all currently pressed keys
+var keys = {};
+
+//Setup WebSocket to the server
+let webSocket = new WebSocket("ws://" + window.location.host + "/ManualControl", "ManualControl");
+
+
+//If we receive a message update our position
+var websocketData = {};
+
+webSocket.onmessage = (message) => {
+  websocketData = JSON.parse(message.data);
+  console.log(message);
+}
+
+//Movement Speed / Distance difference
+var Displacement = 0;
+var TurningHeading = 0;
+
+//Key listeners
+document.addEventListener('keydown', (e) => {
+  if(e.code === "KeyW"){
+    console.log("Forward Command Recieved");
+    webSocket.send(JSON.stringify({"Target_R": 1}));
+    Displacement += 0.01;//Jake Line 2
+  }
+  else if(e.code === "KeyS"){
+    console.log("Back Command Recieved");
+    webSocket.send(JSON.stringify({"Target_R": -1}));
+    Displacement -= 0.01;
+  }
+  else if(e.code === "KeyD"){
+    console.log("Right Turn Command Recieved");
+    webSocket.send(JSON.stringify({"Target_Theta": 1}));
+    TurningHeading += 0.01;
+  }
+  else if(e.code === "KeyA"){
+    console.log("Left Turn Command Recieved");
+    webSocket.send(JSON.stringify({"Target_Theta": -1}));
+    TurningHeading -= 0.01;
+  }
+});
+  
   //define the inital states of the nodeData and its updated verion
   const [nodeData, updateNodeData] = useState([]);
   const [pathData, updatePathData] = useState([]);
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -11,49 +11,5 @@ root.render(
   </React.StrictMode>
 );
 
-//dictionary holding all currently pressed keys
-var keys = {};
-
-
-console.log(window.location.host);
-//Setup WebSocket to the server
-let webSocket = new WebSocket("ws://" + window.location.host + "/ManualControl", "ManualControl");
-
-
-//If we receive a message update our position
-var websocketData = {};
-
-webSocket.onmessage = (message) => {
-  websocketData = JSON.parse(message.data);
-  console.log(message);
-}
-
-//Movement Speed / Distance difference
-var Displacement = 0;
-var TurningHeading = 0;
-
-//Key listeners
-document.addEventListener('keydown', (e) => {
-  if(e.code === "KeyW"){
-    console.log("Forward Command Recieved");
-    webSocket.send(JSON.stringify({"Target_R": 1}));
-    Displacement += 0.01;//Jake Line 2
-  }
-  else if(e.code === "KeyS"){
-    console.log("Back Command Recieved");
-    webSocket.send(JSON.stringify({"Target_R": -1}));
-    Displacement -= 0.01;
-  }
-  else if(e.code === "KeyD"){
-    console.log("Right Turn Command Recieved");
-    webSocket.send(JSON.stringify({"Target_Theta": 1}));
-    TurningHeading += 0.01;
-  }
-  else if(e.code === "KeyA"){
-    console.log("Left Turn Command Recieved");
-    webSocket.send(JSON.stringify({"Target_Theta": -1}));
-    TurningHeading -= 0.01;
-  }
-});
 
 
