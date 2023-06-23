f0c09fcd563f3faa656d56b95ce847bef3365693
omalley242 <omalley242@gmail.com>
2023-06-23 10:32:08 +0100

    auto-testing

--- react-app/src/components/MapContainer/MapContainer.js
+++ react-app/src/components/MapContainer/MapContainer.js
@@ -8,6 +8,34 @@ websocket.onmessage = (message) => {
     console.log(message);
 }
 
+//Movement Speed / Distance difference
+var Displacement = 0;
+var TurningHeading = 0;
+
+//Key listeners
+document.addEventListener('keydown', (e) => {
+  if(e.code === "KeyW"){
+    console.log("Forward Command Recieved");
+    websocket.send(JSON.stringify({"Target_R": 1}));
+    Displacement += 0.01;//Jake Line 2
+  }
+  else if(e.code === "KeyS"){
+    console.log("Back Command Recieved");
+    websocket.send(JSON.stringify({"Target_R": -1}));
+    Displacement -= 0.01;
+  }
+  else if(e.code === "KeyD"){
+    console.log("Right Turn Command Recieved");
+    websocket.send(JSON.stringify({"Target_Theta": 1}));
+    TurningHeading += 0.01;
+  }
+  else if(e.code === "KeyA"){
+    console.log("Left Turn Command Recieved");
+    websocket.send(JSON.stringify({"Target_Theta": -1}));
+    TurningHeading -= 0.01;
+  }
+});
+
 const MapContainer = ({nodeData, pathData}) => {
 
     function setEndNode(id) {
--- react-app/src/index.js
+++ react-app/src/index.js
@@ -12,32 +12,3 @@ root.render(
 );
 
 
-//Movement Speed / Distance difference
-var Displacement = 0;
-var TurningHeading = 0;
-
-//Key listeners
-document.addEventListener('keydown', (e) => {
-  if(e.code === "KeyW"){
-    console.log("Forward Command Recieved");
-    websocket.send(JSON.stringify({"Target_R": 1}));
-    Displacement += 0.01;//Jake Line 2
-  }
-  else if(e.code === "KeyS"){
-    console.log("Back Command Recieved");
-    websocket.send(JSON.stringify({"Target_R": -1}));
-    Displacement -= 0.01;
-  }
-  else if(e.code === "KeyD"){
-    console.log("Right Turn Command Recieved");
-    websocket.send(JSON.stringify({"Target_Theta": 1}));
-    TurningHeading += 0.01;
-  }
-  else if(e.code === "KeyA"){
-    console.log("Left Turn Command Recieved");
-    websocket.send(JSON.stringify({"Target_Theta": -1}));
-    TurningHeading -= 0.01;
-  }
-});
-
-
