a1431a4a995dc4dbafd9055bf5e8c501754c99d7
omalley242 <omalley242@gmail.com>
2023-06-23 05:41:33 +0100

    auto-testing

--- server/Server.js
+++ server/Server.js
@@ -343,7 +343,7 @@ function main_server(database_connection) {Introduction:
     };
   
     app.get('/dijkstras', (req, res) => {     
-        if(EndNodeId != undefined)
+        // if(EndNodeId != undefined)
             dijkstras('0',EndNodeId.toString()).then((shortestPath) => {
                 database_connection.query(`UPDATE Paths SET Colour="green" `, (err) => {
                     if (err) console.log(`Error updating path: ${err.code}`);
