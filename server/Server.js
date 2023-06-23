0e0bc3a6b381c5b9c0b10445af8cfe575fb510b8
Jalexandrou <jalexandrou10@gmail.com>
2023-06-23 03:50:44 +0100

    feat: add dijkstra's

--- server/Server.js
+++ server/Server.js
@@ -254,6 +254,100 @@ function main_server(database_connection) {Introduction:
         // }); 
     }
 
+    // Find closest node
+    let closestNode = (distances, visited) => {
+        // create a default value for shortest
+        let closest = null;
+        
+        // for each node in the distances object
+        for (let node in distances) {
+            // if no node has been assigned to closest yet
+            // or if the current node's distance is smaller than the current closest
+            let currentIsclosest =
+                closest === null || distances[node] < distances[closest];
+                
+            // and if the current node is in the unvisited set
+            if (currentIsclosest && !visited.includes(node)) {
+                // update closest to be the current node
+                closest = node;
+            }
+        }
+        return closest;
+    };
+
+    let findShortestPath = (graph, startNode, endNode) => {
+    
+        // track distances from the start node using a hash object
+        let distances = {};
+        distances[endNode] = "Infinity";
+        distances = Object.assign(distances, graph[startNode]);
+        // track paths using a hash object
+        let parents = { endNode: null };
+
+        for (let child in graph[startNode]) {
+            parents[child] = startNode;
+        }
+        
+        // collect visited nodes
+        let visited = [];
+        // find the nearest node
+        let node = closestNode(distances, visited);
+        
+        // for that node:
+        while (node) {
+            // find its distance from the start node & its child nodes
+            let distance = distances[node];
+            let children = graph[node]; 
+                
+            // for each of those child nodes:
+            for (let child in children) {
+            
+                // make sure each child node is not the start node
+                if (String(child) === String(startNode)) {
+                    continue;
+                } else {
+                    // save the distance from the start node to the child node
+                    let newdistance = distance + children[child];
+                    // if there's no recorded distance from the start node to the child node in the distances object
+                    // or if the recorded distance is shorter than the previously stored distance from the start node to the child node
+                    if (!distances[child] || distances[child] > newdistance) {
+                        // save the distance to the object  
+                            distances[child] = newdistance;
+                        // record the path
+                            parents[child] = node;
+                    } 
+                }
+            }  
+                // move the current node to the visited set
+                visited.push(node);
+                // move to the nearest neighbor node
+                node = closestNode(distances, visited);
+        }
+        
+        // using the stored paths from start node to end node
+        // record the shortest path
+        let shortestPath = [endNode];
+
+        let parent = parents[endNode];
+
+        while (parent) {
+            shortestPath.push(parent);
+            parent = parents[parent];
+        }
+        shortestPath.reverse();
+        
+        // //this is the shortest path
+        // let results = {
+        //     distance: distances[endNode],
+        //     path: shortestPath,
+        // };
+        // // return the shortest path & the end node's distance from the start node
+        // return results;
+
+        return shortestPath;
+    };
+
+    
     app.get('/testdik', (req, res) => {     
         dijkstras(0,3).then((shortestPath) => {
             shortestPath.reduce((lastId, currentId) => {    
@@ -269,34 +363,26 @@ function main_server(database_connection) {Introduction:
     //node-content ID | XCoordinate | YCoordinate | HeadingAlpha | HeadingBeta | HeadingGamma |
 
     // path-content StartId | EndId | Heading From Start | Distance |
-    async function dijkstras(startNodeId, EndNodeId){
-        // From start id to end if, iterate through id's and make key value, id_origin: {id_neighbour: distance, id_neighbour: distance} 
-        // e.g.
-        // let graph = {
-        //     start: { A: 5, B: 2 },
-        //     A: { start: 1, C: 4, D: 2 },
-        //     B: { A: 8, D: 7 },
-        //     C: { D: 6, finish: 3 },
-        //     D: { finish: 1 },
-        //     finish: {},
-        // };
-
+    async function dijkstras(startNodeId, endNodeId){
+        // From start id to end if, iterate through id's and make key value, nodeData = { id_origin: {id_neighbour: distance, id_neighbour: distance} }
+     
         var nodeData = new Object();
 
-        for (node = startNodeId; node <= EndNodeId; node++){
+        for (node = startNodeId; node <= endNodeId; node++){
             let curNode = await promiseQuery(`SELECT * FROM Paths WHERE StartId=${node} OR EndId=${node}`);
             nodeData[node] = JSON.parse(JSON.stringify(curNode));
         }
 
-        console.log(`nodeData:`);
-        console.log(nodeData);
+        // console.log(`nodeData:`);
+        // console.log(nodeData);
 
         for (node in nodeData) {
             let temp_dic = {};
             nodeData[node].forEach(nodepath => temp_dic[((nodepath.StartId == node) ? nodepath.EndId : nodepath.StartId)] = nodepath.Distance);
             nodeData[node] = temp_dic;
         }
-        console.log(nodeData);
+        // console.log(nodeData);
+        findShortestPath(nodeData, startNodeId, endNodeId);
 
     }
 
