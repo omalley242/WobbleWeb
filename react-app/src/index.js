aab3eb0bd185e72eff7685ec1bb810777fd7904b
omalley242 <omalley242@gmail.com>
2023-06-23 02:20:25 +0100

    auto-testing

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-
+//Render the main page
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App/>
